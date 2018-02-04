e6b7c43ad7e79617d0be1f2122d5ec4fc0d3793c
Nik Bornemeier <NikolausRB@gmail.com>
2018-02-04 11:20:06 -0700

    add nav in header and stylize

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,10 +6,17 @@ class App extends Component {
     return (
       <div className="App">
         <header>
+          <div className='name-and-logo'>
           <div className='logo-container'>
             <p>NB</p>
           </div>
           <h1 className="title"><span className='name'>Nik Bornemeier</span> <br/> Frond-end Web Developer</h1>
+          </div>
+          <nav>
+            <a href="/">About</a> 
+            <a href="/">Portfolio</a> 
+            <a href="/">Contact</a> 
+          </nav>
         </header>
 
         <p className="intro">
