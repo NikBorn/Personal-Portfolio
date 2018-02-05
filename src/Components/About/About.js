ce4b06549ee5f563b92ea6dbfee9fb07f18d4871
Nik Bornemeier <NikolausRB@gmail.com>
2018-02-05 16:18:11 -0700

    style about page and add text.

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,7 +2,29 @@ import React from 'react';
 
 const About = () => {
   return (
-    <h2>About</h2>
+    <main className='about-page'>
+      <div className='page-header'>
+        <h3>
+          About Me
+        </h3>
+      </div>
+      <p className='about-text'>
+        I am coming into the tech industry after a successful career in Marketing and Management.  I graduated from 
+        the University of Colorado with a BS in Business with an emphasis in Marketing.  I've held many different job
+        titles in the past including: Manager, Director of Marketing, Practice Manager for a speech therapy practice,
+        Licensing Manager, Delivery Manager, and Product Designer.
+        <br /><br />
+        I am very excited to enter a new chapter of my life by starting out in a completly new field.  I recently
+        graduated from Turing School of Software and Design in Denver, CO with a certificat of completion from their
+        front-end developer program.  Besides learning valuable skills like time and stress management as well as
+        personal and coloaborative, project management; I also learned HTML5, Javascript(Echmascript6), CSS3, React,
+        React-Redux and Express.js.
+        <br /><br />
+        I'm looking to join a collaborative team environment, that will also offer me a path to advancement.  I am 
+        very coachable and someone who enjoys furthering my education as well as teaching others the skils and techniques
+        that I've picked up along the way.
+      </p>
+    </main>
   )
 }
 
