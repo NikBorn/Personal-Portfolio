de3a6b3819e0b42bdefcba33ef3836973fdcbe11
Nik Bornemeier <NikolausRB@gmail.com>
2018-02-06 10:35:41 -0700

    add data for contact and protfolio page and stylize both.

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <h2>Contact</h2>
+    <main>
+      <div className='page-header'>
+        <h3>
+          Contact Me
+        </h3>
+      </div>
+      <div className='contact-page'>
+        <h4>Email: NikolausRB@Gmail.com </h4>
+        <h4>Phone: 720-883-1603</h4>
+        <h4>Slack: @NikBorn</h4>
+        <h4>GitHub: @NikBorn</h4>
+        <h4>LinkedIn Profile</h4>
+      </div>
+    </main>
   )
 }
 
