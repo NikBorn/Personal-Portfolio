fbdd7bc35079005fdb094a5853abb422d71d9ac4
Nik Bornemeier <NikolausRB@gmail.com>
2018-02-09 16:44:17 -0700

    move project overview into a hover state instead of expanding project div.

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
+import './Project.css';
 
 const Project = (props) => {
 
   const projectPreview = `project-preview-${props.name}`
   const projectInfo = props.isSelected === false ? `project-info` : `project-info project-info-expanded`
-  const isExpanded = props.isSelected === false ? `project` : `project project-expanded`
+  // const isExpanded = props.isSelected === false ? `project` : `project project-expanded`
 
   return (
-    <div className={isExpanded} >
+    <div className='project' >
       <h3 className='project-name'>{props.name}</h3>
-      <div className={projectPreview} onClick={() => {
-        props.handleClick(props.name)
-      }}>
+      <div className='project-overview'>
+        <div className={projectInfo}>
+          <a href={props.website} target='_blank'>Website</a>
+          <a href={props.gitHub} target='_blank'>Github</a>
+          <p>Short description about the project.  Talk about the tech stack,challenges, and things that went well.</p>
+        </div>
       </div>
-      <div className={projectInfo}>
-        <a href={props.website} target='_blank'>Website</a>
-        <a href={props.gitHub} target='_blank'>Github</a>
-        <p>Short description about the project.  Talk about the tech stack,challenges, and things that went well.</p>
+      <div className={projectPreview} >
       </div>
+     
     </div>
   )
 }
