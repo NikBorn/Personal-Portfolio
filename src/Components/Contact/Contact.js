3bad06438bf50e7c16b595f9bdcd503c2a122a10
Nik Bornemeier <NikolausRB@gmail.com>
2018-02-12 18:32:23 -0700

    add gitHub link.

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -12,8 +12,7 @@ const Contact = () => {
       <div className='contact-page'>
         <a href='mailto:NikolausRB@gmail.com'>Email Me</a>
         <a href='tel:1-719-201-9493'>Call Me</a>
-        <p><span className='label'>Slack:</span>@NikBorn</p>
-        <p><span className='label'>GitHub:</span>@NikBorn</p> {/* <- make this a GitHub link */}
+        <a href='https://github.com/NikBorn'>GitHub</a> {/* <- make this a GitHub link */}
         <p><span className='label'>LinkedIn:</span></p> {/* <- make this a LinkedIn link */}
       </div>
     </main>
