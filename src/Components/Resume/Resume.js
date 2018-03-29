6ec43a769cb537e96a792087ef287c8856dcf834
Nik Bornemeier <NikolausRB@gmail.com>
2018-03-29 12:55:30 -0600

    add resume component.

---
+++ src/Components/Resume/Resume.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Resume.css';
+
+const Resume = () => {
+  return (
+    <main>
+      <h4>Profile</h4>
+      <p>
+        I am a Denver based web developer with a passion for crea vity and con nued educa on. I am excited to con nue to learn and write complex code. I am currently looking to join a collabora ve developement team in the Denver metro area or Boulder.
+      </p>
+      <h4>Skills</h4>
+      <h3>
+        HTML5 - CSS3 - JavaScript - React - React-Redux - jQuery - Git - GitHub - Agile Development - Node.js - Express.js
+      </h3>
+      <h4>Projects</h4>
+      <p>
+        Palette Picker
+        HTML5, JQUERY, EXPRESS, JEST
+        Palette Picker was an individual Project in which a user can generate a random five color palette and save it to a project to be used later. This was the first project in which we were tasked with crea ng a server and database on the backend.
+        Movie Tracker
+        HTML5, REACT, REACT-REDUX, REACT-ROUTER, SCSS, ASYNC API CALLS, JEST
+        Movie Tacker was a three person group project in which we were tasked with crea ng a simplified front-end version of Ne lix. This was our first experience wri ng a redux web applica on. We created a single page web app that makes asynchronous API calls and displaying new movies with a summary and user ratings.
+      </p>
+      <h4>Education</h4>
+      <p>
+        University of Colorado – BS in Business with an emphasis in Marketing 2005
+      </p>
+      <h4>Experience</h4>
+      <p>
+        Turing School of Software and Design Denver, CO
+        I completed an intensive coding bootcamp in which I learned HTML5, CSS3, Javascript(ES5/ES6), jQuery, React, Redux, Object Oriented and Functional Programming, Test Driven Development, Git/GitHub, Node.js, Express.js, Knex.js, PostgreSQL, and collaborative programming.
+        Practice Manager, Therapies of the Rockies Denver/Colo. Springs, CO
+        I managed everything outside of speech therapy for a practice that started with one office in Aurora. I was the second employee and grew the company to 6 therapists and 2 office managers in just under three years, while also increasing revenues from approx. $70,000/yr. to over $450,000/yr.
+        Manager, Advantage Logistics Denver, CO
+        Working directly with Walmart I managed a team of over 30 delivery drivers, for Walmarts online grocery delivery system. Hired, trained, and managed all employees and handled all day to day actvities.
+      </p>
+    </main>
+  )
+
+}
+
+export default Resume;
