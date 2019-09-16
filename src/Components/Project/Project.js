df7eb37c64c339d669173c546c4c40927b5ae089
Nik Bornemeier <NikolausRB@gmail.com>
2019-09-16 10:48:02 -0600

    add additional projects and some conditional rendering for projects without a website.

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -6,27 +6,34 @@ import PropTypes from 'prop-types';
 const Project = (props) => {
 
   const projectPreviewClassNames = `project-preview-${props.name} project-preview`;
-
+  const projectLinks = () => {
+    if (props.website) {
+      return (
+        <div className='website-container'>
+          <a href={props.website} target='_blank'>Website</a>
+          <a href={props.gitHub} target='_blank'>Github</a>
+        </div>
+      );
+      
+    } else {
+      return (
+        <div className='website-container'>
+          <a href={props.gitHub} target='_blank'>Github</a>
+        </div>
+      );
+    }
+  };
   return (
     <div className='project' >
       <div className={projectPreviewClassNames} >
       </div>
       <h3 className='project-name'>{props.name}</h3>
-
       <div className='project-info'>
-
-        <div className='website-container'>
-          <a href={props.website} target='_blank'>Website</a>
-          <a href={props.gitHub} target='_blank'>Github</a>
-        </div>
-
+        {projectLinks()}
         <p>TECH: {props.tech}</p>
-        {/* <br /> */}
         <br/>
         <p>DESCRIPTION: {props.description}</p>
-
       </div>
-     
     </div>
   );
 };
--- src/content/projects.js
+++ src/content/projects.js
@@ -6,21 +6,62 @@ const projects = [
     tech: 'Express, knex, PostgreSQL, jQuery, HTML5, CSS3, mocha, chai',
     description: 'PalettePicker was a solo project and the fist project in which we created a backend database and server.  I found knex and express to be very intuitive and I really enjoyed my first venture into the backend of a website!'
   },
-  { name: 'MovieTracker', 
+  { 
+    name: 'MovieTracker', 
     gitHub: 'https://www.github.com/NikBorn/movie-tracker', 
-    website: '', 
     tech: 'React-redux, Async Javascript, CSS3, HTML5, javascript-ES6', 
-    description: 'Movie tracker was not only the first time we used React-redux, but also the first time we had ever used React-router and async javascript to interact with an API.  This was a group project and overall I feel it turned out very well and was one of my most positive group experiences while I was at Turing, I was able to learn so much from both of my collaborators.'},
-  { name: 'Headcount', 
+    description: 'Movie tracker was not only the first time we used React-redux, but also the first time we had ever used React-router and async javascript to interact with an API.  This was a group project and overall I feel it turned out very well and was one of my most positive group experiences while I was at Turing, I was able to learn so much from both of my collaborators.'
+  },
+  { 
+    name: 'Headcount', 
     gitHub: 'https://www.github.com/NikBorn/Headcount2.0', 
     website: 'https://nb-headcount.herokuapp.com/',
     tech: 'React, CSS3, HTML5, javascript-ES6, Victory, enzyme',
-    description: 'Headcount was the first individual React project that I completed.  I also wanted to challenge myself implement some data visualization, so I ended up using Victory, which I really liked and I look forward to implementing in future projects as well.'},
-  { name: 'BYOB',
+    description: 'Headcount was the first individual React project that I completed.  I also wanted to challenge myself implement some data visualization, so I ended up using Victory, which I really liked and I look forward to implementing in future projects as well.'
+  },
+  { 
+    name: 'BYOB',
     gitHub: 'https://github.com/NikBorn/byob',
     website: 'https://nb-byob.herokuapp.com/',
     tech: 'jQuery, Express.js, Knex, PostgreSQL, chai, mocha',
-    description: 'This was a two person group assignment in which we were tasked with building a backend using Express.js and Knex.  This project also served as an introduction for Jason Web Tokens.  If you enter an email ending in turing.io it will grant you admin rights and you can make requests(POST/PUT/PATCH/DELETE) via Postman.'}
+    description: 'This was a two person group assignment in which we were tasked with building a backend using Express.js and Knex.  This project also served as an introduction for Jason Web Tokens.  If you enter an email ending in turing.io it will grant you admin rights and you can make requests(POST/PUT/PATCH/DELETE) via Postman.'
+  },
+  {
+    name: 'FlappyBird',
+    gitHub: 'https://github.com/NikBorn/gametime',
+    website: 'https://nb-byob.herokuapp.com/',
+    tech: 'JavaScript, HTML Canvas',
+    description: 'This project was our first introduction to more complex JavaScript and using HTML Canvas. We were tasked with re-creating a game of our choice. We chose to create a clone of the game Flappy Bird, but designed to mimic the general appearence of a Turing classroom during lunch break! (Burritos and Mountain Dew).'
+  },
+  {
+    name: 'IdeaBox',
+    gitHub: 'https://github.com/NikBorn/IdeaBox1.2',
+    website: 'https://nikborn.github.io/IdeaBox1.2/',
+    tech: 'JavaScript, HTML, CSS',
+    description: 'For this project we were tasked with creating an app allowing a user to input, store, and search all their great ideas.'
+  },
+  {
+    name: '2DoBox',
+    gitHub: 'https://github.com/NikBorn/2DoBox-Pivot',
+    website: 'https://nikborn.github.io/2DoBox-Pivot/',
+    tech: 'JavaScript, HTML, CSS',
+    description: 'For this project we took an existing code base based on the IdeaBox project and pivoted to create a Todo List.  We also included the ability for a user to rate their Todo items and into five different catagories filter their ideas based on a search term, as well as hide or show their completed todo items.'
+  },
+  {
+    name: 'NumberGuesser',
+    gitHub: 'https://github.com/NikBorn/Number-Guesser-II',
+    website: 'https://nikborn.github.io/Number-Guesser-II/',
+    tech: 'JavaScript, HTML, CSS',
+    description: 'This was our first app project.  We created an application where a user guesses a number between 1-100.  With each correct guess the number range goes up by 10.'
+  },
+  {
+    name: 'StaticComp3',
+    gitHub: 'https://github.com/NikBorn/NB-static-comp-challenge-3',
+    website: 'https://nikborn.github.io/NB-static-comp-challenge-3/',
+    tech: 'HTML, CSS',
+    description: 'This was strictly a static layout challenge.  We had to recreate the desktop view exactly and then we were challenged with how to handle the layout on a moble view.'
+  }
+
 ];
 
 
